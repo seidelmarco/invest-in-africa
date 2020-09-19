a49d21683995ad2ef3aecc5b95d017ff1dbb0d3b
Marco <marco.seidel@gmx.de>
2020-09-19 11:09:05 +0200

    post_edit und Formular

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
 
