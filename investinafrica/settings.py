084dc839758544aeabfa50560c44236cb472a113
Marco <marco.seidel@gmx.de>
2020-11-29 20:51:48 +0100

    AfricanArt - Upload video

--- investinafrica/settings.py
+++ investinafrica/settings.py
@@ -131,13 +131,15 @@ USE_TZ = True
 # http://127.0.0.1:8000/static/css/style.css basically think this as a reference to static files.
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = BASE_DIR / 'media' # is equal to os.path.join(BASE_DIR, 'media/')
+
+#MEDIA_ROOT = BASE_DIR / 'media' # is equal to os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Location of static file
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
